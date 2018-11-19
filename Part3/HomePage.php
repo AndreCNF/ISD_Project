85fd87d51e7279ca681ceb8420d4cbb19fbf144a
BrunoAlfredo <36961934+BrunoAlfredo@users.noreply.github.com>
2018-11-19 15:18:18 +0000

    Static html homepage and searchpage

---
+++ Part3/HomePage.php
@@ -0,0 +1,15 @@
+<html>
+	<body>
+		<h1 style="color:MediumSeaGreen;">Welcome to Vetpital</h1>
+		<h3>Would you like to</h3>
+		<form action="Search.php" method="get">
+			<p><input type="submit" value="Search"/></p>
+		</form>
+		<form action="HomePage.php" method="get">
+			<p><input type="submit" value="Register a consult"/></p>
+		</form>
+		<form action="HomePage.php" method="get">
+			<p><input type="submit" value="Register a blood test"/></p>
+		</form>
+	</body>
+</html>
---
+++ Part3/Search.php
@@ -0,0 +1,15 @@
+<html>
+	<body>
+		<h1 style="color:MediumSeaGreen;">Vetpital</h1>
+		<form action="WelcPage.php" method="post">
+			<h3>Please introduce the following informations</h3>
+			<p>Animal name: </p>
+			<input type="text" name="animal_name"/>
+			<p>Owner name: </p> 
+			<input type="text" name="owner_name"/>
+			<p>Client VAT: </p> 
+			<input type="number" name="client_vat"/>
+			<p><input type="submit" value="Get"/></p>
+		</form>
+	</body>
+</html>
