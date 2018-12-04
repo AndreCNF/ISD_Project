4f65a51a3cac1dc752e8e7d64208df9c1ffdf686
AndreCNF <andrecnf@gmail.com>
2018-12-04 13:17:36 +0000

    More images, less commented code

--- Part3/code/ResultSearch.php
+++ Part3/code/ResultSearch.php
@@ -39,8 +39,6 @@
     }
 	if ($result->rowCount() == 0)
 	{
-		#echo("<h4>There was no animal found:    <a href=\"new_animal.php?animal={$animal_name}&");
-		#echo("owner={$owner_name}\"> Register new animal </a> </h4>");
 		echo("<h4>There was no animal found <form action='new_animal.php' method='post'>\n<p><input type='submit' value='Register New Animal'/></p>\n</form></h4>");
 
 	}else{
@@ -50,7 +48,6 @@
 		foreach($result as $row)
 		{
 			echo("<tr align='center'>");
-			#echo("<td><a href=\"display_consults.php?animal={$row['animal']}&owner={$row['owner']}\"> {$row['animal']} </a></td>");
 			echo("<td align='center'><form action='display_consults.php' method='post'>\n");
 			echo("<input type='hidden' name='animal' value='{$row['animal']}'/>\n");
 			echo("<input type='hidden' name='owner' value='{$row['owner']}'/>\n");
@@ -66,33 +63,6 @@
 		echo("</table>");
 		echo("<h4>Select the animal that you are looking for</h4>");
 		$result = NULL;
-
-		# Check if the client is registered in the database
-		/*$sql = "SELECT * FROM client WHERE VAT = :v";
-		$st = $connection->prepare($sql);
-		$st->bindParam(':v',$clientVAT);
-		if ($st == FALSE){
-			$info = $connection->errorInfo();
-			echo("<p>Error: {$info[2]}</p>");
-			exit();
-		}else{
-			$res = $st->execute();
-			if ($res == FALSE){
-				$info = $connection->errorInfo();
-				echo("<p>Error: {$info[2]}</p>");
-				exit();
-			}
-			$r = $st->fetch();
-			if ($st->rowCount() == 0)
-			{	
-				# Present the option to create new client
-				#echo("<h4>This client is not registered:    <a href=\"new_client.php?\"> Register new client </a> </h4>");
-				echo("<h4>This client is not registered please  <form action='NewClient.php' method='post'>\n<p><input type='submit' value='Register new Client'/></p>\n</form></h4>");
-			}else{
-				echo("<h4>Select the animal that you are looking for</h4>");
-			}
-		}*/
-		
 	}
 	
 	$connection = null;
